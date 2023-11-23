64e102b9d1191583a1c25a4fa84b7949b275d860
honeydanji <129818881+honeydanji@users.noreply.github.com>
2023-11-24 00:14:50 +0900

    [Silver II] Title: 최대 힙, Time: 1120 ms, Memory: 110868 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,39 @@
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCase = sc.nextInt();
+        int input;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < testCase; i++) {
+            input = sc.nextInt();
+
+            if (input == 0) {
+                // 비어 있는 경우
+                if(pq.isEmpty()) {
+                    sb.append(input + "\n");
+                    continue;
+                }
+                // 제일 큰값 삭제와 동시에 출력
+                int output = pq.poll();
+                if (i != testCase - 1) {
+                    sb.append(output + "\n");
+                }else {
+                    sb.append(output);
+                }
+            }else {
+                pq.add(input);
+            }
+        }
+
+        System.out.println(sb);
+    }    
+}
