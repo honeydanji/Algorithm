27a72efa910717353470f74f121a3eb5a38f1d6a
honeydanji <129818881+honeydanji@users.noreply.github.com>
2023-12-11 22:59:17 +0900

    [Silver V] Title: 영화감독 숌, Time: 384 ms, Memory: 89448 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n= sc.nextInt();
+		int num=0;
+		while(n>0) { 
+			num++;
+			if(String.valueOf(num).contains("666")) { 
+				n--;
+			}
+			
+		}
+		System.out.println(num);
+	}
+	
+}
